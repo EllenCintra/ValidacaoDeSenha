dbe072dbfb243e5624ad788284e91dc10425d079
ellen <ellenc.monteiro@gmail.com>
2025-05-14 08:52:41 -0300

    estrutura basica

---
+++ src/main/java/com/challenge/validaSenha/ValidaSenhaApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.validaSenha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidaSenhaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValidaSenhaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/validaSenha/ValidaSenhaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.validaSenha;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidaSenhaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
